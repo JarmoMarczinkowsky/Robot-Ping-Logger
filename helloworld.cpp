028d8e873d60c3b060e84d19defa94ef8b48d819
JarmoMarczinkowsky <jarmo2002@yahoo.nl>
2025-11-19 22:49:19 +0100

    Added logging in csv file

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "icmplib.h"
+#include <fstream>
 
 int pingRepeat(std::string address, std::string &resolved);
 
-int pingRepeat(std::string address, std::string &resolved)
+int pingRepeat(std::string address, std::string &resolved, std::ofstream &fileOut)
 {
     int ret = EXIT_SUCCESS;
 
@@ -12,40 +13,46 @@ int pingRepeat(std::string address, std::string &resolved)
     
     auto result = icmplib::Ping(address, ICMPLIB_TIMEOUT_1S);
     
+
     switch (result.response) {
-    case icmplib::PingResponseType::Failure:
+        case icmplib::PingResponseType::Failure:
         std::cout << "Network error." << std::endl;
         ret = EXIT_FAILURE;
         break;
-
-    case icmplib::PingResponseType::Timeout:
+        
+        case icmplib::PingResponseType::Timeout:
         std::cout << "Request timed out." << std::endl;
         break;
-
-    default:
+        
+        default:
         std::cout << "Reply from " << static_cast<std::string>(result.address) << ": ";
         
         switch (result.response) 
         {
-        
+            
             case icmplib::PingResponseType::Success:
-            std::cout << "time=" << result.delay;
-            if (result.address.GetType() != icmplib::IPAddress::Type::IPv6) {
-                std::cout << " TTL=" << static_cast<unsigned>(result.ttl);
-            }
-            break;
-        
+                std::cout << "time=" << result.delay;
+                if (result.address.GetType() != icmplib::IPAddress::Type::IPv6) {
+                    std::cout << " TTL=" << static_cast<unsigned>(result.ttl);
+                }
+                fileOut << address << "," << "Success" << "," << result.delay << "," << static_cast<unsigned>(result.code) << "," << static_cast<unsigned>(result.ttl) << "\n";
+                break;
+            
             case icmplib::PingResponseType::Unreachable:
                 std::cout << "Destination unreachable.";
+                fileOut << address << "," << "Unreachable" << ",,,\n";
                 break;
-        
+            
             case icmplib::PingResponseType::TimeExceeded:
                 std::cout << "Time exceeded.";
+                fileOut << address << "," << "TimeExceeded" << ",,,\n";
                 break;
-        
+            
             default:
                 std::cout << "Response not supported.";
-        
+                fileOut << address << "," << "Unsupported" << ",,,\n";
+                break;
+            
         }
         
         std::cout << std::endl;
@@ -76,13 +83,27 @@ int main(int argc, char *argv[])
     }
 
     int ret = EXIT_SUCCESS;
+
+    std::ofstream fileOut;
+    fileOut.open("ping_log.csv", std::ios::out | std::ios::app);
+    if (!fileOut.is_open()) {
+        std::cerr << "Error opening file for writing." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    //check if file is empty to write header
+    fileOut.seekp(0, std::ios::end);
+    if (fileOut.tellp() == 0)
+    {
+        fileOut << "address,response_type,delay_ms,code,ttl\n";
+    }
     
     for(int i = 0; i < 4; ++i)
     {
-        ret = pingRepeat(address, resolved);
+        ret = pingRepeat(address, resolved, fileOut);
     }
 
-    
+    fileOut.close();
     return ret;
 }
 
